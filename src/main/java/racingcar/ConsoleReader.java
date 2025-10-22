be682098bc227b7c40ba46715feffaef8c09c173
도미닉 <kiljh@me.com>
2025-10-22 23:48:49 +0900

    feat: 사용자의 입력을 받아서 반환하는 메서드 구현

---
+++ src/main/java/racingcar/ConsoleReader.java
@@ -0,0 +1,10 @@
+package racingcar;
+
+import java.util.Scanner;
+
+public class ConsoleReader {
+    public String readCarNames() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+}
