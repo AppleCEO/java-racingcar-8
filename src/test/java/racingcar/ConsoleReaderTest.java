755dafb5afeed5d4844b5de8b736a225d42d1586
도미닉 <kiljh@me.com>
2025-10-23 17:12:39 +0900

    test: 사용자 입력을 안내할 테스트에 시도할 횟수 안내 문구 테스트 추가

--- src/test/java/racingcar/ConsoleReaderTest.java
+++ src/test/java/racingcar/ConsoleReaderTest.java
@@ -27,6 +27,7 @@ class ConsoleReaderTest extends NsTest {
             run("pobi,woni", "1");
             ConsoleReader.readInput();
             assertThat(output()).contains("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+            assertThat(output()).contains("시도할 횟수는 몇 회인가요?");
         });
     }
 
