63f0e111d1cd220a57fbe6a7cfadcd96552c2020
도미닉 <kiljh@me.com>
2025-10-23 17:08:50 +0900

    refactor: 차이름부터 읽도록 각 메서드는 private로 변경하고 다른 메서드 생성

--- src/main/java/racingcar/ConsoleReader.java
+++ src/main/java/racingcar/ConsoleReader.java
@@ -3,12 +3,20 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 
 public class ConsoleReader {
-    public static String readCarNames() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        return Console.readLine();
+    public static String carNames;
+    public static String moveCount;
+
+    public static void readInput() {
+        readCarNames();
+        readMoveCount();
     }
 
-    public static String readMoveCount() {
-        return Console.readLine();
+    private static void readCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        carNames = Console.readLine();
+    }
+
+    private static void readMoveCount() {
+        moveCount = Console.readLine();
     }
 }
