3187e691f60c11d4accaf49edb6510117da5035a
도미닉 <kiljh@me.com>
2025-10-23 17:00:29 +0900

    feat: 이동 횟수를 읽는 메서드 추가

--- src/main/java/racingcar/ConsoleReader.java
+++ src/main/java/racingcar/ConsoleReader.java
@@ -1,11 +1,14 @@
 package racingcar;
 
-import java.util.Scanner;
+import camp.nextstep.edu.missionutils.Console;
 
 public class ConsoleReader {
     public static String readCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        Scanner scanner = new Scanner(System.in);
-        return scanner.nextLine();
+        return Console.readLine();
+    }
+
+    public static String readMoveCount() {
+        return Console.readLine();
     }
 }
