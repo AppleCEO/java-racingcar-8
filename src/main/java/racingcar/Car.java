d428b4dec45ecb027a42d1ccfaab9e928e8ea36b
도미닉 <kiljh@me.com>
2025-10-24 12:29:16 +0900

    feat: 자동차 역할을 담당할 별도 객체 생성

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,11 @@
+package racingcar;
+
+public class Car {
+    public String name;
+    public int position;
+
+    public Car(String name) {
+        this.name = name;
+        this.position = 0;
+    }
+}
