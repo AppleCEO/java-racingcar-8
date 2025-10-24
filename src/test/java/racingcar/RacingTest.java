47cdcc3f2e2a75f24fcaf3ef4246bc0ad87d329e
도미닉 <kiljh@me.com>
2025-10-24 09:55:38 +0900

    test: 차 이름과 횟수를 입력받아 start를 통해 레이스를 진행하는 과정 테스트

---
+++ src/test/java/racingcar/RacingTest.java
@@ -0,0 +1,25 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class RacingTest extends NsTest {
+    @Test
+    void 레이스_테스트() {
+        assertSimpleTest(() -> {
+            Racing racing = new Racing(new String[]{"pobi","woni"}, 1);
+            racing.start();
+            assertThat(output()).contains("pobi");
+            assertThat(output()).contains("woni");
+        });
+    }
+
+    @Override
+    public void runMain() {
+    }
+}
