f5d0374daba45545c625961e6752ea8bbac326f3
도미닉 <kiljh@me.com>
2025-10-24 10:00:46 +0900

    feat: 무작위로 차가 이동되고 이동할 때마다 위치가 출력되는 기능 구현

--- src/main/java/racingcar/ConsoleOutput.java
+++ src/main/java/racingcar/ConsoleOutput.java
@@ -8,4 +8,15 @@ public class ConsoleOutput {
     public static void printMoveCountInputGuide() {
         System.out.println("시도할 횟수는 몇 회인가요?");
     }
+
+    public static void printRacingStart() {
+        System.out.println("\n실행 결과");
+    }
+
+    public static void printRacingStateByMove(String[] carNames, int[] carPositions) {
+        for (int carIndex = 0; carIndex < carNames.length; carIndex++) {
+            System.out.println(carNames[carIndex] + " : " + "-".repeat(carPositions[carIndex]));
+        }
+        System.out.println("\n");
+    }
 }
--- src/main/java/racingcar/Racing.java
+++ src/main/java/racingcar/Racing.java
@@ -1,11 +1,32 @@
 package racingcar;
 
+import java.util.Random;
+
 public class Racing {
     private String[] carNames;
     private int moveCount;
+    private int[] carPositions;
+    private Random random = new Random();
 
     public Racing(String[] carNames, int moveCount) {
         this.carNames = carNames;
         this.moveCount = moveCount;
+        this.carPositions = new int[carNames.length];
+    }
+
+    public void start() {
+        for (int moveIndex = 0; moveIndex < moveCount; moveIndex++) {
+            for (int carIndex = 0; carIndex < this.carPositions.length; carIndex++) {
+                move(carIndex);
+            }
+            ConsoleOutput.printRacingStateByMove(carNames, carPositions);
+        }
+    }
+
+    private void move(int carIndex) {
+        int randomNumber = random.nextInt(10);
+        if (randomNumber >= 4) {
+            carPositions[carIndex] += 1;
+        }
     }
 }
