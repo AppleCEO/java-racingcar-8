5ac45b3fb2bd202e73edbdd64bf1e3d8bbd55862
도미닉 <kiljh@me.com>
2025-10-24 09:16:01 +0900

    feat: 차 이름들과 이동 횟수를 가지고 레이싱을 하는 객체 생성

---
+++ src/main/java/racingcar/Racing.java
@@ -0,0 +1,11 @@
+package racingcar;
+
+public class Racing {
+    private String[] carNames;
+    private int moveCount;
+
+    public Racing(String[] carNames, int moveCount) {
+        this.carNames = carNames;
+        this.moveCount = moveCount;
+    }
+}
