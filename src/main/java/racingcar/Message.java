44f23b1e01f2d454b887dbdbc48d36d9cd22c318
도미닉 <kiljh@me.com>
2025-10-27 19:18:04 +0900

    feat: 요구사항과 다르게 공백이 들어간 문자열에 공백 추가

--- src/main/java/racingcar/Message.java
+++ src/main/java/racingcar/Message.java
@@ -3,7 +3,7 @@ package racingcar;
 public enum Message {
     // --- 입력 요청 메시지 ---
     PROMPT_CAR_NAMES("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
-    PROMPT_RACE_ROUNDS("시도할 횟수는 몇회인가요?"),
+    PROMPT_RACE_ROUNDS("시도할 횟수는 몇 회인가요?"),
 
     // --- 실행 결과 메시지 ---
     RESULT_HEADER("\n실행 결과"),
